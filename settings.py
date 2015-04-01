9ca93f9e847b5bcfc14d74b2d910c30dbe85ca58
Emin Mastizada <emin@mastizada.com>
2015-04-01 11:41:33 +0300

    ubuntu.mozillaz.org as temp domain

--- settings.py
+++ settings.py
@@ -98,7 +98,8 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["www.ubuntu-az.org", "ubuntu-az.org"]
+# ALLOWED_HOSTS = ["www.ubuntu-az.org", "ubuntu-az.org"]
+ALLOWED_HOSTS = ["ubuntu.mozillaz.org"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
